8365cb206838b853ecaff5b675c7e9ea80520fb7
morneydeetlefs <59366837+morneydeetlefs@users.noreply.github.com>
2026-04-09 07:09:32 +0200

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,19 @@
+const CACHE_NAME = 'grok-shopping-v1';
+const urlsToCache = [
+    './',
+    'index.html'   // or shopping-list.html if that's your filename
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(urlsToCache))
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => response || fetch(event.request))
+    );
+});
